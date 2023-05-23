48790dc43cbf1eb9b731acb8c16dc90151663bc4
Simran Inamdar <125954439+simraninamdar09@users.noreply.github.com>
2023-05-23 20:50:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -143,7 +143,7 @@ if st.button('prevention type'):
 
     prediction=pickled_model.predict(data.drop(' class',axis=1))
     
-    if pk.any()==1:
+    if pk.any()==0:
         prediction = "Bankruptcy"
     else:
         prediction = "Non-Bankruptcy"
