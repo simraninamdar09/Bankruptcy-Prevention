7fabffb880f1109fdadd23344b0ef70f17bb8b62
Simran Inamdar <125954439+simraninamdar09@users.noreply.github.com>
2023-05-24 20:28:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -57,7 +57,7 @@ x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.3,random_state=42
 #num_trees = 60
 #AdaBoost Classification
 kfold=KFold(n_splits=5,random_state=72,shuffle=True)
-model = AdaBoostClassifier(random_state=8)
+model = [AdaBoostClassifier(n_estimators=60,random_state=8)]
 #model.fit(x_train, y_train)
 result_ab = cross_val_score(model, x, y, cv=kfold)
 result_ab.mean()
