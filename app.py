06cabc544ed584dcf2e8854a3c46d0e6ce9e79c5
Simran Inamdar <125954439+simraninamdar09@users.noreply.github.com>
2023-05-28 11:05:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -33,6 +33,15 @@ result_ab = cross_val_score(model_ab, x, y, cv=kfold)
 #Accuracy
 print(result_ab.mean())
 
+st.markdown(
+   f”””
+   <style>
+   p {
+   background-image: url(‘img1.jfif’);
+   }
+   </style>
+   ”””,
+   unsafe_allow_html=True)
 
 
 filename = 'final_Adaboost_model.pkl'
